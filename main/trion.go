ef547c7add82163ded3cc770e5ba742a8bb4e256
Nathaniel Kofalt <nathaniel@kofalt.com>
2013-09-09 19:54:16 -0500

    MAXIMUM FAST.

--- main/trion.go
+++ main/trion.go
@@ -3,10 +3,12 @@ package main
 import (
 	. "polydawn.net/dockctrl/trion"
 	. "fmt"
+	"os"
 )
 
 func main() {
 	config := FindConfig(".")
 	CID := Run(config)
 	Println(CID)
+	os.Exit(0) //GOTTA GO FAST. SIX TIMES AS MUCH FAST.
 }
