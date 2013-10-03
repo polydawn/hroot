82b0565ce19551b090fee5b8e1c6a79244f9d2bd
Eric Myhre <hash@exultant.us>
2013-10-02 19:45:34 -0500

    add Container to crocker.
    as with Dock which maps fairly directly to a docker daemon, Container maps to a container managed by a daemon.   so far, just starting to replicate operations trion could already perform, but with a more OO design and without exposing the implementation details of the exec library.

---
+++ crocker/container.go
@@ -0,0 +1,56 @@
+package crocker
+
+/*
+	Launches a new Container in the given Dock.
+*/
+func LaunchContainer(dock *Dock, command string) *Container {
+	dockRun := dock.cmd()("run")
+
+	// set up cidfile
+	CIDfilename := CreateCIDfile()
+	dockRun = dockRun("-cidfile", CIDfilename)
+
+	//TODO: moar conf
+
+	// launch
+	dockRun()
+	getCID := PollCid(CIDfilename)
+
+	return &Container{
+		dock: dock,
+		id: <-getCID,
+	}
+}
+
+/*
+	Refers to a Container process.
+	The container has been started and has an ID.
+	Methods on this struct can be used to inspect and manipulate the container.
+*/
+type Container struct {
+	// reference to the daemon we launched this container with
+	dock *Dock
+
+	// id of this container
+	id string
+}
+
+/*
+	Waits for the container's main process to exit (i.e., wraps `docker wait`).
+*/
+func (c *Container) Wait() {
+	//TOOD:FUTURE: consider if wait/attach should be just built-in when launching a container, and this method just wraps checks against a promise-pattern.
+	c.dock.cmd()("wait", c.id)()
+}
+
+/*
+	Discards the container state and filesystem (i.e., wraps `docker rm`).
+
+	After this method completes, many other inspection methods on this Container become
+	invalid and will error, because the backing data has been, well, purged.
+
+	This will error if called on a still-running container.
+*/
+func (c *Container) Purge() {
+	c.dock.cmd()("rm", c.id)()
+}
