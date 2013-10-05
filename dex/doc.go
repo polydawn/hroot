58f8fca715283900d9cfef03f3566d2b9364cb0f
Eric Myhre <hash@exultant.us>
2013-10-05 18:24:22 -0500

    Start new dex.Graph to abstract away the implementation of the image storage system.
    When done, it should be entirely invisible outside of that package that the git interface happens to be implmented via execing.

---
+++ dex/doc.go
@@ -0,0 +1,5 @@
+
+/*
+	Dex provides a mechanism for storing filesystem images of the kind used by docker, using git as a datastore/deduplication/history/transport system.
+*/
+package dex
--- dex/graph.go
+++ dex/graph.go
@@ -1,14 +1,37 @@
-package fab
+package dex
 
 import (
 	. "polydawn.net/gosh/psh"
+	"path/filepath"
 )
 
-var GraphDir = "/dockctrl.graph/"
-var GraphGit = Sh("git")(DefaultIO)(Opts{Cwd: GraphDir})
+type Graph struct {
+	/*
+		Absolute path to the base/working dir for of the graph git repository.
+	*/
+	dir string
 
-var GraphGit_Cleanse = func() {
-	GraphGit("reset")()
-	GraphGit("checkout", ".")()
-	GraphGit("clean", "-xf")()
+	/*
+		Cached command template for exec'ing git with this graph's cwd.
+	*/
+	cmd Shfn
+}
+
+func NewGraph(dir string) *Graph {
+	dir, err := filepath.Abs(dir)
+	if err != nil { panic(err); }
+
+	return &Graph{
+		dir: dir,
+		cwd: Sh("git")(DefaultIO)(Opts{Cwd: dir}),
+	}
+}
+
+/*
+Wipes uncommitted changes in the git working tree.
+*/
+func (g *Graph) Cleanse() {
+	g.cmd("reset")()
+	g.cmd("checkout", ".")()
+	g.cmd("clean", "-xf")()
 }
