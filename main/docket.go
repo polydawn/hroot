e526771507015438208242a593ad7ec20fc2e899
Nathaniel Kofalt <nathaniel@kofalt.com>
2013-10-18 01:45:14 -0500

    Added help command & default help action.

--- main/docket.go
+++ main/docket.go
@@ -11,11 +11,20 @@ var EXIT_BADARGS = 1
 var EXIT_PANIC = 2
 
 func main() {
+	//Go-flags is a little too clever with sub-commands.
+	//To keep the help-command parity with git & docker / etc, check for 'help' manually before args parse
+	if len(os.Args) < 2 || os.Args[1] == "help" {
+		parser.WriteHelp(os.Stdout)
+		os.Exit(0)
+	}
+
+	//Parse for command & flags, and exit with a relevant return code.
 	_, err := parser.Parse()
 	if err != nil {
 		os.Exit(EXIT_BADARGS)
+	} else {
+		os.Exit(0)
 	}
-	os.Exit(0)
 }
 
 func init() {
