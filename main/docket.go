a7f046b5f68a89062e16eb0038fee3437b4e6213
Nathaniel Kofalt <nathaniel@kofalt.com>
2013-11-10 22:35:29 -0600

    Add stack-trace handling to main function. Fixes #14.

--- main/docket.go
+++ main/docket.go
@@ -1,17 +1,32 @@
 package main
 
 import (
+	"fmt"
+	"os"
 	"github.com/jessevdk/go-flags"
 	. "polydawn.net/docket/commands"
-	"os"
 )
 
 var parser = flags.NewNamedParser("docket", flags.Default | flags.HelpFlag)
 
-var EXIT_BADARGS = 1
-var EXIT_PANIC = 2
+const EXIT_BADARGS = 1
+const EXIT_PANIC = 2
+
+// print only the error message (don't dump stacks).
+// unless any debug mode is on; then don't recover, because we want to dump stacks.
+func panicHandler() {
+	if len(os.Getenv("DEBUG")) == 0 {
+		if err := recover(); err != nil {
+			fmt.Fprintf(os.Stderr, "%s\n", err)
+			fmt.Println("\n" + "Docket crashed! This could be a problem with docker or git, or docket itself." + "\n" + "To see more about what went wrong, turn on stack traces by running:" + "\n\n" + "export DEBUG=1" + "\n\n" + "Feel free to contact the developers for help:" + "\n" + "https://github.com/polydawn/docket" + "\n")
+			os.Exit(EXIT_PANIC)
+		}
+	}
+}
 
 func main() {
+	defer panicHandler()
+
 	//Go-flags is a little too clever with sub-commands.
 	//To keep the help-command parity with git & docker / etc, check for 'help' manually before args parse
 	if len(os.Args) < 2 || os.Args[1] == "help" {
