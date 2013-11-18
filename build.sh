5c59197da4fe5ed4bf6ca359cd3ee73261f303bf
Eric Myhre <hash@exultant.us>
2013-11-17 20:52:31 -0600

    wannabe makefile in shell.
    (aren't you glad the go toolchain is so zero-arg that you don't even need a makefile?  ohwait)

--- build.sh
+++
@@ -1,9 +0,0 @@
-# Where is this script located?
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-# Build the project
-(
-	cd $DIR ;
-	export GOPATH="$PWD"/.gopath/ ;
-	go build -race -o docket polydawn.net/docket/main
-)
