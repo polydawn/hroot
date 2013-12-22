c2bd5e953d63196404efd747001b5b51bc8b16e7
Nathaniel Kofalt <nathaniel@kofalt.com>
2013-12-21 18:06:54 -0600

    You know what's cool? Toml parsers. You know what's cooler? Reading the docs for the Toml parsers.

--- conf/format.go
+++ conf/format.go
@@ -12,43 +12,43 @@ const GraphFolder    = "graph"
 //Image and parent image
 type Image struct {
 	//What image to use
-	Name        string     `toml:name`
+	Name        string     `toml:"name"`
 
 	//What image to build from
-	Upstream    string     `toml:upstream`
+	Upstream    string     `toml:"upstream"`
 
 	//What the upstream image is called in the docker index
-	Index       string     `toml:index`
+	Index       string     `toml:"index"`
 }
 
 //A container's settings
 type Container struct {
 	//What command to run
-	Command     []string   `toml:command`
+	Command     []string   `toml:"command"`
 
 	//Which folder to start
-	Folder      string     `toml:folder`
+	Folder      string     `toml:"folder"`
 
 	//Run in privileged mode?
-	Privileged  bool       `toml:privileged`
+	Privileged  bool       `toml:"privileged"`
 
 	//Array of mounts (each an array of strings: hostfolder, guestfolder, "ro"/"rw" permission)
-	Mounts      [][]string `toml:mounts`
+	Mounts      [][]string `toml:"mounts"`
 
 	//What ports do you want to forward? (each an array of ints: hostport, guestport)
-	Ports       [][]string `toml:ports`
+	Ports       [][]string `toml:"ports"`
 
 	//Do you want to use custom DNS servers?
-	DNS         []string   `toml:dns`
+	DNS         []string   `toml:"dns"`
 
 	//Attach interactive terminal?
-	Attach      bool       `toml:attach`
+	Attach      bool       `toml:"attach"`
 
 	//Delete when done?
-	Purge       bool       `toml:purge`
+	Purge       bool       `toml:"purge"`
 
 	//Env variables (each an array of strings: variable, value)
-	Environment [][]string `toml:environment`
+	Environment [][]string `toml:"environment"`
 }
 
 //Localize a container object to a given folder
@@ -88,13 +88,13 @@ var DefaultContainer = Container {
 //Docket configuration
 type Configuration struct {
 	//The image struct
-	Image    Image                `toml:image`
+	Image    Image                `toml:"image"`
 
 	//The settings struct
-	Settings Container            `toml:settings`
+	Settings Container            `toml:"settings"`
 
 	//A map of named targets, each representing another set of container settings
-	Targets  map[string]Container `toml:targets`
+	Targets  map[string]Container `toml:"targets"`
 }
 
 func (c *Configuration) GetTarget(target string) Container {
